9c096258d1edeedb4f3977bffba379bbede6ae80
Jon-a-than <2140145186@qq.com>
2023-05-21 13:34:37 +0800

    🎉 feat: 初始化

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+import { createAlova } from 'alova'
+import GlobalFetch from 'alova/GlobalFetch'
+import VueHook from 'alova/vue'
+
+export const alovaInstance = createAlova({
+  statesHook: VueHook,
+
+  requestAdapter: GlobalFetch(),
+
+  responded: (response: Response) => response.json(),
+})
